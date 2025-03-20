773e6c14ec5ed41dce6d8686138ac4b5668b6e2e
A-4S <dev.forsythe@gmail.com>
2025-03-20 10:52:27 -0400

    Updated test_curry.py; additional tests

--- test/test_curry.py
+++ test/test_curry.py
@@ -6,6 +6,17 @@ def test_curry():
     def sum_all(a: int, b: int, c: int):
         return a + b + c
 
+    # args test
     assert sum_all(1)(2)(3) == 6
     assert sum_all(1, 2)(3) == 6
     assert sum_all(1, 2, 3) == 6
+
+    # kwargs test
+    assert sum_all(a=1)(b=2)(c=3) == 6
+    assert sum_all(b=1)(c=2)(a=3) == 6
+    assert sum_all(c=1)(a=2)(b=3) == 6
+
+    # mixed test
+    assert sum_all(1)(2)(c=3) == 6
+    assert sum_all(1, b=2)(c=3) == 6
+    assert sum_all(a=1, b=2, c=3) == 6
