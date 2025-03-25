132febb3b36934328d8f678f9b64cb857f0d8a92
A-4S <dev.forsythe@gmail.com>
2025-03-25 15:04:53 +0000

    Updated test_curry.py; added more tests

--- src/currypy/curry.py
+++ src/currypy/curry.py
@@ -1,5 +1,5 @@
 from functools import partial, wraps
-from inspect import signature
+from inspect import signature, getcallargs, getfullargspec
 from typing import Callable
 
 
@@ -9,12 +9,72 @@ def curry(f: Callable):
     """
 
     def count_args(f: partial, *args, **kwargs):
-        return len(f.args) if isinstance(f, partial) else len(args) + len(kwargs)
+        # print(signature(f))
+        
+        match isinstance(f, partial):
+            case True:
+                # print(f"args: {f.args}, kwargs: {f.keywords}")
 
-    @wraps(f)
+                return len(f.args) + len(f.keywords)
+            case False:
+                # print(f"args: {args}, kwargs: {kwargs}") 
+
+                return len(args) + len(kwargs)
+    
+    def examine(f: Callable, *args, **kwargs):
+        part = partial(f, *args, **kwargs)
+        spec = getfullargspec(part)
+
+        print(f"args: {spec.args}")
+        print(f"kwonlyargs: {spec.kwonlyargs}")
+        print(f"kwonlydefaults: {spec.kwonlydefaults}")
+        print(f"signature: {signature(partial(f, *args, **kwargs))}")
+    
+    def check_args(f: Callable, *args, **kwargs):
+        part = partial(f, *args, **kwargs)
+        spec = getfullargspec(part)
+
+        examine(f, *args, **kwargs)
+
+        # check = True if len(spec.args) == 0 and (len(spec.kwonlyargs) == len(spec.kwonlydefaults)) else False
+
+        match spec.kwonlydefaults:
+            case None:
+                check = True if len(spec.args) == 0 else False
+            case _:
+                check = True if len(spec.args) == 0 and len(spec.kwonlyargs) == len(spec.kwonlydefaults) else False
+        
+        match len(spec.args) == 0:
+            case True:
+                match len(spec.kwonlyargs) == len(spec.kwonlydefaults):
+                    case True:
+                        return partial(f, *args, **kwargs)
+
+        print(check)
+        print('==============')
+
+        return check
+
+    # @wraps(f)
     def wrapper(*args, **kwargs):
-        if count_args(f, *args, **kwargs) >= len(signature(f).parameters):
-            return f(*args, **kwargs)
+        # if check_args(f, *args, **kwargs) == True:
+        #     match getfullargspec(partial(f, *args, **kwargs)).kwonlydefaults:
+        #         case None:
+        #             return f(*args, **kwargs)
+        #         case _:
+        #             return partial(f, *args, **kwargs)
+
+        # spec = getfullargspec(partial(f, *args, **kwargs))
+
+        # match spec.defaults:
+        #     case None:
+        #         match len(spec.args) == 0:
+        #             case True:
+        #                 return f(*args, **kwargs)
+
+        def apply_defaults(f: Callable, *args, **kwargs):
+            params = signature(f).parameters
+
 
         return curry(partial(f, *args, **kwargs))
 
