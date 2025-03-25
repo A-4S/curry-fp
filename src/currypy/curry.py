69507c5935e114249d7430572070e087e359b163
A-4S <dev.forsythe@gmail.com>
2025-03-25 16:52:31 +0000

    Updated curry.py; refactoring

--- src/currypy/curry.py
+++ src/currypy/curry.py
@@ -1,5 +1,5 @@
 from functools import partial, reduce
-from inspect import signature, getargs
+from inspect import BoundArguments, signature, getargs
 from typing import Callable
 
 
@@ -11,29 +11,39 @@ def curry(f: Callable | partial):
     def count_args(f: Callable | partial, *args, **kwargs):
         def count_items(*args):
             return reduce(lambda a, c: a + len(c), args, 0)
-
-        return count_items(f.args, f.keywords) if hasattr(f, 'args') else count_items(args, kwargs)
+        
+        match hasattr(f, 'args'):
+            case True:
+                return count_items(f.args, f.keywords)
+            case False:
+                return count_items(args, kwargs)
     
     def apply_defaults(f: partial):
         def filter_args(f: partial):
             return filter(lambda x: x != ..., f.args)
 
-        sig = signature(f.func)
-        ba = sig.bind_partial(*filter_args(f), **f.keywords)
+        def bind_args(f: partial):
+            return signature(f.func).bind_partial(*filter_args(f), **f.keywords)
+        
+        def partial_with_defaults(f: partial, ba: BoundArguments):
+            return ba.apply_defaults() or partial(f.func, *ba.args, **ba.kwargs)
 
-        return ba.apply_defaults() or partial(f.func, *ba.args, **ba.kwargs)
+        return partial_with_defaults(f, bind_args(f))
 
     def wrapper(*args, **kwargs):
         def is_base_case(f: partial, *args, **kwargs):
             return len(getargs(f.func.__code__)) == count_args(f, *args, **kwargs)
         
-        f_a = partial(f, *args, **kwargs)
-        f_b = apply_defaults(f_a) if ... in args else f_a
+        def select_default_args(f: partial, *args):
+            return apply_defaults(f) if ... in args else f
+
+        def case_result(f: partial, *args, **kwargs):        
+            match is_base_case(f, *args, **kwargs):
+                case True:
+                    return f()
+                case False:
+                    return curry(f)
         
-        match is_base_case(f_b, *args, **kwargs):
-            case True:
-                return f_b()
-            case False:
-                return curry(f_b)
+        return case_result(select_default_args(partial(f, *args, **kwargs), *args), *args, **kwargs)
 
     return wrapper
